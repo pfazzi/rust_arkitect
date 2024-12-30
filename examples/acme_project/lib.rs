6875f53ae719c397866ac1b6e7afe980b37ad6b3
Patrick Luca Fazzi <patrick91@live.it>
2024-12-30 13:08:25 +0100

    fmt
    Signed-off-by: Patrick Luca Fazzi <patrick91@live.it>

--- examples/acme_project/lib.rs
+++ examples/acme_project/lib.rs
@@ -8,38 +8,38 @@ pub mod architecture_tests {
         let project = Project::from_relative_path(file!(), "./../../../acme_project/src");
 
         #[rustfmt::skip]
-    let rules = ArchitecturalRules::define()
-        .component("Utils")
-            .located_at("crate::utils")
-            .allow_external_dependencies(&[])
-            .must_not_depend_on_anything()
+        let rules = ArchitecturalRules::define()
+            .component("Utils")
+                .located_at("crate::utils")
+                .allow_external_dependencies(&[])
+                .must_not_depend_on_anything()
 
-        .component("Errors")
-            .located_at("crate::errors")
-            .allow_external_dependencies(&[])
-            .must_not_depend_on_anything()
+            .component("Errors")
+                .located_at("crate::errors")
+                .allow_external_dependencies(&[])
+                .must_not_depend_on_anything()
 
-        .component("EventSourcing")
-            .located_at("crate::event_sourcing")
-            .allow_external_dependencies(&["std::fmt"])
-            .must_not_depend_on_anything()
+            .component("EventSourcing")
+                .located_at("crate::event_sourcing")
+                .allow_external_dependencies(&["std::fmt"])
+                .must_not_depend_on_anything()
 
-        .component("Application")
-            .located_at("crate::application")
-            .allow_external_dependencies(&["std"])
-            .may_depend_on(&["Domain", "Errors", "Utils"])
+            .component("Application")
+                .located_at("crate::application")
+                .allow_external_dependencies(&["std"])
+                .may_depend_on(&["Domain", "Errors", "Utils"])
 
-        .component("Domain")
-            .located_at("crate::domain")
-            .allow_external_dependencies(&["std", "chrono", "log", "serde", "uuid"])
-            .may_depend_on(&["EventSourcing", "Errors"])
+            .component("Domain")
+                .located_at("crate::domain")
+                .allow_external_dependencies(&["std", "chrono", "log", "serde", "uuid"])
+                .may_depend_on(&["EventSourcing", "Errors"])
 
-        .component("Infrastructure")
-            .located_at("crate::infrastructure")
-            .allow_external_dependencies(&["serde"])
-            .may_depend_on(&["Domain", "Application", "Errors", "EventSourcing", "Utils"])
+            .component("Infrastructure")
+                .located_at("crate::infrastructure")
+                .allow_external_dependencies(&["serde"])
+                .may_depend_on(&["Domain", "Application", "Errors", "EventSourcing", "Utils"])
 
-        .finalize();
+            .finalize();
 
         let result = Arkitect::ensure_that(project).complies_with(rules);
 
