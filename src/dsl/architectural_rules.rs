874eae94ef0a9295d638e8dbe38d8a700a401168
Patrick Luca Fazzi <patrick91@live.it>
2025-01-17 23:40:24 +0100

    Update DSL
    Signed-off-by: Patrick Luca Fazzi <patrick91@live.it>

--- src/dsl/architectural_rules.rs
+++ src/dsl/architectural_rules.rs
@@ -89,7 +89,7 @@ impl ArchitecturalRules<SubjectDefined> {
         }
     }
 
-    pub fn add_rule(self, rule: Box<dyn Rule>) -> ArchitecturalRules<RulesDefined> {
+    pub fn rule(self, rule: Box<dyn Rule>) -> ArchitecturalRules<RulesDefined> {
         let mut rules = self.rules;
         rules.push(rule);
 
@@ -151,7 +151,7 @@ impl ArchitecturalRules<RulesDefined> {
         }
     }
 
-    pub fn add_rule(self, rule: Box<dyn Rule>) -> ArchitecturalRules<RulesDefined> {
+    pub fn rule(self, rule: Box<dyn Rule>) -> ArchitecturalRules<RulesDefined> {
         let mut rules = self.rules;
         rules.push(rule);
 
@@ -232,7 +232,7 @@ mod tests {
         let rules = ArchitecturalRules::define()
             .rules_for_crate("application")
                 .may_depend_on(&["my_app", "domain"])
-                .add_rule(custom_rule)
+                .rule(custom_rule)
             .build();
 
         assert_eq!(rules.len(), 2);
